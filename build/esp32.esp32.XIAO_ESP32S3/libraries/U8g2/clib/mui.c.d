e3df95d4ede58a6ac994d60ff7ef97ea605a4903
danghieu7424 <danghieu7424storage@gmail.com>
2026-08-31 15:36:25 +0700

    build: generate build artifacts for ESP32-S3 project

---
+++ build/esp32.esp32.XIAO_ESP32S3/libraries/U8g2/clib/mui.c.d
@@ -0,0 +1,3 @@
+D:\all_projects\rust\rust\display_oled\build\esp32.esp32.XIAO_ESP32S3\libraries\U8g2\clib\mui.c.o: \
+ C:\Users\dangh\Documents\Arduino\libraries\U8g2\src\clib\mui.c \
+ C:\Users\dangh\Documents\Arduino\libraries\U8g2\src\clib\mui.h
--- build_flash.bat
+++ build_flash.bat
@@ -4,14 +4,14 @@ setlocal
 REM [CHÚ THÍCH] Khai báo các biến cục bộ trỏ đến công cụ và thư mục dự án
 set CLI_PATH="C:\Users\dangh\AppData\Local\Programs\Arduino IDE\resources\app\lib\backend\resources\arduino-cli.exe"
 set BUILD_DIR=.\build\esp32.esp32.XIAO_ESP32S3
-set ELF_FILE=%BUILD_DIR%\face_rbot.ino.elf
+set ELF_FILE=%BUILD_DIR%\display_oled.ino.elf
 
 REM [CHÚ THÍCH] ĐÃ LOẠI BỎ LỆNH RMDIR ĐỂ GIỮ LẠI CACHE (GIỐNG ARDUINO IDE)
 
 REM [CHÚ THÍCH] BƯỚC 1: BIÊN DỊCH TĂNG DẦN (INCREMENTAL COMPILE)
 REM arduino-cli sẽ tự kiểm tra và chỉ dịch lại những gì bạn vừa sửa đổi.
 echo [INFO] Bat dau bien dich nhanh (Incremental Compile)...
-%CLI_PATH% compile --fqbn esp32:esp32:XIAO_ESP32S3 --build-path %BUILD_DIR% face_rbot.ino
+%CLI_PATH% compile --fqbn esp32:esp32:XIAO_ESP32S3 --build-path %BUILD_DIR% display_oled.ino
 
 REM [CHÚ THÍCH] BƯỚC 2: KIỂM TRA LỖI (FAIL-FAST)
 REM Nếu quá trình build có lỗi C++, biến %ERRORLEVEL% sẽ khác 0. Kịch bản sẽ dừng ngay lập tức.
