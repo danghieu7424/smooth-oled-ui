5ef12bf2b4551203a09c7bda22a0b1e0781c33ed
danghieu7424 <danghieu7424storage@gmail.com>
2026-09-04 21:52:04 +0700

    Remove project_id prefix

--- backend/src/bin/test_db.rs
+++ backend/src/bin/test_db.rs
@@ -2,15 +2,13 @@ use rusqlite::Connection;
 
 fn main() {
     let conn = Connection::open("./storages/database/otahub_core.db").unwrap();
-    let _ = conn.execute("ALTER TABLE users ADD COLUMN suid TEXT", []);
-    let mut stmt = conn.prepare("PRAGMA table_info(users);").unwrap();
-    let rows = stmt.query_map([], |row| {
-        let name: String = row.get(1).unwrap();
-        Ok(name)
-    }).unwrap();
-
-    println!("Columns in users table:");
-    for row in rows {
-        println!("- {}", row.unwrap());
-    }
+    let _ = conn.execute(
+        "UPDATE projects SET project_id = REPLACE(project_id, '007Rlq-', '') WHERE project_id LIKE '007Rlq-%'",
+        [],
+    );
+    let _ = conn.execute(
+        "UPDATE firmwares SET project_id = REPLACE(project_id, '007Rlq-', '') WHERE project_id LIKE '007Rlq-%'",
+        [],
+    );
+    println!("Database migrated.");
 }
