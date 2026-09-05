ca7a001a3c90a8ba2dbc7668dec17b0f501874b7
danghieu7424 <danghieu7424storage@gmail.com>
2026-09-05 12:32:07 +0700

    fix(build): remove double quotes around CLI_PATH in batch script

---
+++ firmware/build/esp32.esp32.XIAO_ESP32S3/libraries/U8g2/clib/u8x8_u16toa.c.d
@@ -0,0 +1,3 @@
+D:\all_projects\rust\rust\display_oled\firmware\build\esp32.esp32.XIAO_ESP32S3\libraries\U8g2\clib\u8x8_u16toa.c.o: \
+ C:\Users\dangh\Documents\Arduino\libraries\U8g2\src\clib\u8x8_u16toa.c \
+ C:\Users\dangh\Documents\Arduino\libraries\U8g2\src\clib\u8x8.h
--- firmware/build_flash.bat
+++ firmware/build_flash.bat
@@ -12,7 +12,7 @@ REM [CHÚ THÍCH] ĐÃ LOẠI BỎ LỆNH RMDIR ĐỂ GIỮ LẠI CACHE (GIỐNG
 REM [CHÚ THÍCH] BƯỚC 1: BIÊN DỊCH TĂNG DẦN (INCREMENTAL COMPILE)
 REM arduino-cli sẽ tự kiểm tra và chỉ dịch lại những gì bạn vừa sửa đổi.
 echo [INFO] Bat dau bien dich nhanh (Incremental Compile)...
-"%CLI_PATH%" compile --fqbn esp32:esp32:XIAO_ESP32S3:PartitionScheme=default_8MB --build-path "%BUILD_DIR%" firmware.ino
+"%CLI_PATH:"=%" compile --fqbn esp32:esp32:XIAO_ESP32S3:PartitionScheme=default_8MB --build-path "%BUILD_DIR%" firmware.ino
 
 REM [CHÚ THÍCH] BƯỚC 2: KIỂM TRA LỖI (FAIL-FAST)
 REM Nếu quá trình build có lỗi C++, biến %ERRORLEVEL% sẽ khác 0. Kịch bản sẽ dừng ngay lập tức.
